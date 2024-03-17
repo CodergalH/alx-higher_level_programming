4004f4bbcdca5623d84683c4e78067e2548ce77b
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-17 09:27:00 -0700

    add to linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * insert_node - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	if (head == NULL);
+		return (NULL)
+
+	listint_t *new_node
+		= malloc(sizeof(listint_t));
+
+	new_node->n = number;
+	new_node->next = head->next;
+	head->next = new_node;
+
+	return(new_node)
+}
