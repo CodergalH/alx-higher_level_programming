bef2196552761b2bbe2883315dde5a92d31beb71
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-17 08:44:11 -0700

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -7,5 +7,5 @@ def print_last_digit(number):
         last_digit = number % 10
         last_digit *= -1
 
-    print("{}".format(last_digit))
+    print("{}".format(last_digit), end="")
     return last_digit
