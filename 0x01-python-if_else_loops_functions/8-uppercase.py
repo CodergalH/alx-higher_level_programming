ee86d5819441663fa93bf86e433ad9334a0dd575
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-17 07:41:52 -0700

    print uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,10 +6,14 @@ def uppercase(str):
     for element in range(0, index):
         char_num = ord(str[element])
 
+        if element == index:
+            index += 1
+            str += " "
+
         if (char_num >= 97 and char_num <= 122):
             char_num -= 32
 
         print("{:c}".format(char_num), end="")
 
-        if (element == index - 1 or element == index):
-            print(" ")
+        if (element == index - 1):
+            print()
