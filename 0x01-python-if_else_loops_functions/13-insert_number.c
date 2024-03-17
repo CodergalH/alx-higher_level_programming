947f2169f5e01472a99770f100b5ba25324badb1
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-17 16:50:04 -0700

    add to linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,8 +19,13 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	while (prev->next->n < number)
-		prev = prev->next;
+		new_node = prev->next;
 
+	while (prev->n > number)
+	{
+		new_node->n = number;
+		new_node->next = prev;
+		prev->next = new_node;
 	if (prev == NULL)
 		prev = new_node;
 
