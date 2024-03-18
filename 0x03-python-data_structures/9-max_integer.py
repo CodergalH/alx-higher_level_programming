82ac8af4491f4cc409fa1cd91f510f473bc3997a
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-18 09:05:05 -0700

    return max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list[]):
+    if my_list:
+        new_list = sorted(my_list)
+        ret = newlist[len(new_list) - 1]
+        return ret
+    else:
+        return None
