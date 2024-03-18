6cf8a7696810eebebcb25f6408ab03b242ba7089
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-18 02:32:06 -0700

    print list of int

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        type = isinstance(mylist[i], int)
+        type = isinstance(my_list[i], int)
         if type is not True:
             continue
         print("{}".format(my_list[i]))
