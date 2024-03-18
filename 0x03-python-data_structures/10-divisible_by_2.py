e08552273e2752a823455d2213595c9cd1011c4a
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-18 15:41:56 -0700

    check for divisibility

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,5 +2,6 @@
 def divisible_by_2(my_list=[]):
     newlist = []
     for i in my_list:
-        newlist.append[i % 2 == 0]
+        num = my_list.index(i)
+        newlist.insert(num, i % 2 == 0)
     return newlist
