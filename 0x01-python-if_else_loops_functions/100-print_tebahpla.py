74c2926fa7c19941642c81cb3fed3435ebdf12f9
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-17 17:38:09 -0700

    print reverse alphabets

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def rev_alphabet():
-    for i in range(122, 98, -1):
+    for i in range(122, 96, -1):
         if (i % 2) != 0:
             i -= 32
 
         print("{:c}".format(i), end="")
+
+rev_alphabet()
