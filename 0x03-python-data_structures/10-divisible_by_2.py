d97d55654ed966161e643f51bef4f0bdf534a84e
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-18 10:20:59 -0700

    check for divisibility

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in range(len(my_list)):
-        newlist[i] = my_list[i] % 2 == 0
+    for i in my_list:
+        newlist = i % 2 == 0
     return newlist
