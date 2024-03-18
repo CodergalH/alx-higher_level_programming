cee3831c96c44c8fc4470e5fac8e9f1b52da053e
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-18 07:42:57 -0700

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                print("{:d} ".format(matrox[i][j]))
+        for i in matrix:
+            for j in i:
+                print("{:d} ".format(matrix[i][j]))
 
             print()
