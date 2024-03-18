77a67e8fbce4c65acc8c2ee2edc170536b0f55e2
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-18 16:43:17 -0700

    check for palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,11 +10,32 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *beginning;
+	int counter;
 
 	beginning = *head;
+	counter = 0;
+	while (beginning != NULL)
+	{
+		beginning = beginning->next;
+		counter++;
+	}
 
-	if (beginning == NULL)
-		return (1);
-	else
-		return (0);
+	int i = 0, j;
+	listint_t *front, *rear;
+
+	while (i != counter / 2)
+	{
+		front = rear = beginning;
+		for (j = 0; j < i; j++)
+			front = front->next;
+
+		for (j = 0; j < counter - (i + 1); j++)
+			rear = rear->next;
+
+		if (front->data != rear->data)
+			return (0);
+
+		i++;
+	}
+	return (1);
 }
