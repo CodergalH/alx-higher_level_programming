2ade196e1f6d41673b1ee454c712af20098523fa
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-18 16:18:40 -0700

    check for palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - check if list is empty
+ * @head: first node
+ *
+ * Return: 0 if not, 1 if list is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *beginning;
+
+	beginning = *head;
+
+	if (beginning == NULL)
+		return (1)
+	else
+		return (0)
+}
