2dc71247ff0a9312ad3c04f34708ec89d60904c0
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-20 16:09:53 -0700

    mak dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,16 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 if __name__ == "__main__":
     import sys
 
-    cpy = argv
-    argn = len(cpy)
-
+    argn = len(sys.argv)
     print("{:d} ".format(argn - 1), end="")
+
     if argn > 2:
         print("arguments:")
-        for i in range(1, argn):
-            print("{:d}: {}".format(i, cpy[i]))
-    elif argn == 1:
-        print("arguments.")
     elif argn == 2:
         print("argument:")
-        for i in range(1, argn):
-            print("{:d}: {}".format(i, cpy[i]))
     else:
-        print()
+        print("argument.")
+
+    for i in range(1, argn):
+        print("{:d}: {}".format(i, sys.argv[i]))
