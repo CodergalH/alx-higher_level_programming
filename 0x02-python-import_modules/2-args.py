4bb950d6748643aa3a091342db4c970f5ae415c6
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-20 15:40:54 -0700

    mak dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
     print("{:d} ".format(argn), end="")
     if argn == 1:
         print("argument:")
+        for i in range(argn):
+            print("{:d}: {}".format(i + 1, cpy[i]))
     elif argn == 0:
         print("arguements.")
     else:
         print("arguments:")
-
-    for i in range(argn):
-        print("{:d}: {}".format(i + 1, cpy[i]))
+        for i in range(argn):
+            print("{:d}: {}".format(i + 1, cpy[i]))
