8466a827b018a0da1f6d38316849483d8a4ea15e
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-20 18:31:10 -0700

    write from bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-
-
 def magic_calculation(a, b):
     """Calculates on numbers."""
     from magic_calculation_102 import add, sub
@@ -10,6 +7,7 @@ def magic_calculation(a, b):
 
         for i in range(4, 6):
             c = add(c, i)
+
         return (c)
 
     else:
