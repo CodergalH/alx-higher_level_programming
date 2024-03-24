9e5e0672c7792d412e625a1ee61b4fee655380df
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-24 06:30:00 -0700

    highest score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if bool(a_dictionary):
-        highest =  max(a_dictionary, key=a_dictionary.get())
+        highest = max(a_dictionary, key=a_dictionary.get())
         return highest
     else:
         return None
