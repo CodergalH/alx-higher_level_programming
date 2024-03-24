f78d5f085baebcdba1bbb8cb2be144ef89862ad8
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-24 06:15:34 -0700

    highest score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    newlist = list(a_dictionary)
-    for i in range(len(newlist)):
-        for x in range(i):
-            if a_dictionary[newlist[i]] > a_dictionary[newlist[x]]:
-                temp = a_dictionary[newlist[i]]
-                a_dictionary[newlist[i]] = a_dictionary[newlist[x]]
-                a_dictionary[newlist[x]] = temp
+    if bool(a_dictionary):
+        newlist = list(a_dictionary)
+        for i in range(len(newlist)):
+            for x in range(i):
+                if a_dictionary[newlist[i]] > a_dictionary[newlist[x]]:
+                    temp = a_dictionary[newlist[i]]
+                    a_dictionary[newlist[i]] = a_dictionary[newlist[x]]
+                    a_dictionary[newlist[x]] = temp
 
-    newlist = list(a_dictionary)
-    return a_dictionary[newlist[0]]
+        newlist = list(a_dictionary)
+        return newlist[0]
+    else:
+        return None
