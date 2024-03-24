1fb27418e1b54dceb5aeb0f8c173d0c39aa787b8
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-24 06:28:47 -0700

    highest score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if bool(a_dictionary):
-        newlist = list(a_dictionary)
-        for i in range(len(newlist)):
-            for x in range(i):
-                if a_dictionary[newlist[i]] > a_dictionary[newlist[x]]:
-                    temp = a_dictionary[newlist[i]]
-                    a_dictionary[newlist[i]] = a_dictionary[newlist[x]]
-                    a_dictionary[newlist[x]] = temp
-
-        newlist = list(a_dictionary)
-        return newlist[0]
+        highest =  max(a_dictionary, key=a_dictionary.get())
+        return highest
     else:
         return None
