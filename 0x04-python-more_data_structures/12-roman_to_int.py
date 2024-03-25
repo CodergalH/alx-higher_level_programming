a5b598eaf1edb0d891df8880611d5fef75f812c8
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-03-25 07:13:31 -0700

    text

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romanum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    if isinstance(roman_string, str):
+        str_list = list(roman_string)
+
+        for i in range(len(str_list)):
+            place = romanum[str_list[i]]
+
+            if i + 1 < len(str_list) - 1:
+                nextp = romanum[str_list[i + 1]]
+
+                if place < nextp:
+                    place = nextp - place
+                    i += 2
+
+            num += place
+
+        return num
+    else:
+        return 0
