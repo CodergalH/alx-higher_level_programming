dcb07aa91396762517b3060c91637ab356d4cec5
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-03 14:07:11 +0100

    average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+def weight_average(my_list=[]):
+    if  len(my_list) == 0:
+        return 0
+    
+    for i in range(len(my_list)):
+        num = 1
+        for j in range(len(my_list[i])):
+            num *= my_list[i][j]
+        
+        total += num
+
+    avg = 0
+    for i in range(len(my_list)):
+        avg += list(my_list[i]).pop()
+
+    average = total / avg
+
+    return  average
