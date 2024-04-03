717696772317ab9a25fc2546d6a4ac492a175dff
Hadarah <ohwojeheriglorious608@gmail.com>
2024-04-03 12:07:14 +0100

    corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,24 +2,22 @@
 def roman_to_int(roman_string):
     romanum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    if isinstance(roman_string, str):
-        str_list = list(roman_string)
-        num = 0
-        lenght = len(str_list) - 1
-
-        for i in range(lenght - 1):
-            place = romanum[str_list[i]]
-            next = i + 1
-
-            if (next < lenght) or (next == lenght):
-                nextp = romanum[str_list[i + 1]]
-
-                if place < nextp:
-                    place = nextp - place
-                    i += 2
-
-            num += place
-
-        return num
-    else:
+    if not isinstance(roman_string, str):
         return 0
+    
+    str_list = list(roman_string)
+    num = 0
+    lenght = len(str_list) - 1
+
+    for i in range(lenght - 1):
+        place = romanum[str_list[i]]
+        next = i + 1
+        nextp = romanum[str_list[next]]
+
+        if (next < lenght) and (place < nextp):
+                place *= -1
+
+        num += place
+
+    return num
+
