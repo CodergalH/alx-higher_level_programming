313fd4545860348716e695e2f04dae03b3b520a1
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-03 12:38:32 +0100

    corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     romanum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    if not isinstance(roman_string, str):
+    if isinstance(roman_string, str):
         str_list = list(roman_string)
         num = 0
         lenght = len(str_list) - 1
