8d49826331a6e054d626a880b930db922690a85e
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-06 13:31:23 +0100

    corrected

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,13 +2,13 @@
 def weight_average(my_list=[]):
     if  len(my_list) == 0:
         return 0
-    
+
     total = 0
     for i in range(len(my_list)):
         num = 1
         for j in range(len(my_list[i])):
             num *= my_list[i][j]
-        
+
         total += num
 
     avg = 0
@@ -17,4 +17,4 @@ def weight_average(my_list=[]):
 
     average = total / avg
 
-    return  average
+    return average
