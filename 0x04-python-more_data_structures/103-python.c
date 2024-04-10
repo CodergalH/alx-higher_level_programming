38858693dda1b4c0e7fc205800b91dc7adba2ca3
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-10 21:55:47 +0100

    COMMIT

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -52,7 +52,7 @@ void print_python_list(PyObject *p)
     Py_ssize_t size = PyList_GET_SIZE(p);
 
     printf("[*] Python list info\n");
-    printf("[*] size of the Python list = %lu\n", size);
+    printf("[*] Size of the Python List = %lu\n", size);
     printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
     for (i = 0; i < ((PyVarObject *)p)->ob_size; i++)
