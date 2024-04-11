5ef82348d6c976ce1861614c5fd3c53dc2a6f5f8
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-11 10:55:16 +0100

    2-safe_print_list_integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+def safe_print_list_integers(my_list=[], x=0):
+    ret = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]))
+                ret += 1
+            else:
+                i += 1
+    except (IndexError, TypeError):
+        pass
+    return ret
