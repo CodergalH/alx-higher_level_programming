4101aea28f66576fa8fd5b2f6976da69b47afa01
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-11 09:45:20 +0100

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def Safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        print("%d".format(i), end=" ")
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+    except IndexError:
+        pass
+    return i + 1
