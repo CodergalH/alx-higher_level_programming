30114e6800aa115ae1fa59041dc59fceeb476981
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-11 11:07:58 +0100

    correct now

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             else:
                 i += 1
         print(end="\n")
-    except (IndexError, TypeError):
+    except TypeError:
         print(end="")
     return ret
