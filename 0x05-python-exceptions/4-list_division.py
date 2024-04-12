ff0e5452650ec51d25e50cf757e85149c083fe1a
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-12 12:55:09 +0100

    4-list_division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,7 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("wrong type")
                 result = 0
             finally:
-                new_list.append[result]
+                new_list.append(result)
     except:
         pass
     finally:
