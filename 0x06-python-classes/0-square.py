f2afdb3f6c4a1b0d1a3e7dd942008ceb43569b7b
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-17 17:55:06 +0100

    correct blank lines

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
-
 """
 Author: Glorious Oyovwuvwe Ohwojeheri <gloriousohwojeheri608@gmail.com>
 Purpose: Define a Square class
 """
+
+
 class Square(object):
     """ A class represents / defines a square.
     """
