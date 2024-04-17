a72eab9f696430358f7063306c768370d7d27b0e
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-17 18:14:40 +0100

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Author: Glorious Oyovwuvwe Ohwojeheri <gloriousohwojeheri608@gmail.com>
+Purpose: Define a Square class
+"""
+
+
+class Square(object):
+    """ A class represents / defines a square.
+    """
+
+    def __init__(self, size):
+        self._square__size = size
