862199145dfa9933d4c0a3e87b40b5e324163783
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-18 08:43:56 +0100

    corr

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -84,7 +84,7 @@ class Square(object):
             print()
         else:
             if self._Square__position[1] != 0:
-                print(" " * self._Square__position[1], end="")
+                print(" " * self._Square__position[1], end="\r")
 
             for i in range(self._Square__size):
                 for x in range(self._Square__position[0]):
