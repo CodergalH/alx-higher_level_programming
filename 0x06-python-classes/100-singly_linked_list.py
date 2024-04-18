4ed4802e33b68c9c1abf8fb0dc163527cd200d06
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-18 10:47:46 +0100

    100-

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -11,14 +11,17 @@ class Node:
     Private instance attribute: data:
         property def data(self): to retrieve it
         property setter def data(self, value): to set it:
-            data must be an integer, otherwise raise a TypeError exception with the message data must be an integer
+            data must be an integer, otherwise raise a TypeError exception 
+            with the message data must be an integer
 
     Private instance attribute: next_node:
         property def next_node(self): to retrieve it
         property setter def next_node(self, value): to set it:
-            next_node can be None or must be a Node, otherwise raise a TypeError exception with the message next_node must be a Node object
+            next_node can be None or must be a Node, otherwise raise a 
+            TypeError exception with the message next_node must be a Node object
 
-    Instantiation with data and next_node: def __init__(self, data, next_node=None):
+    Instantiation with data and next_node:
+        def __init__(self, data, next_node=None):
     """
 
     def __init__(self, data, next_node=None):
@@ -38,7 +41,7 @@ class Node:
     @property
     def data(self):
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         if type(value) is not int:
@@ -64,8 +67,11 @@ class SinglyLinkedList:
         Should be printable:
             print the entire list in stdout
             one node number by line
-        Public instance method: def sorted_insert(self, value): that inserts a new Node into the correct sorted position in the list (increasing order)
+        Public instance method: def sorted_insert(self, value): that inserts 
+        a new Node into the correct sorted position in the list 
+        (increasing order)
     """
+
 
     def __init__(self):
         self.__head = None
@@ -80,8 +86,10 @@ class SinglyLinkedList:
         :param value: The integer that will be added to the list.
         :type value: int
         """
+
+
         new_node = Node(value)
-        if self.__head == None:
+        if self.__head is None:
             self.__head = new_node
         else:
             current = self.__head
