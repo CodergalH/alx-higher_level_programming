e68230db0dc8b7e29120f818519c391ca5c1788a
Ohwojeheri Glorious <ohwojeheriglorious608@gmail.com>
2024-04-18 09:50:56 +0100

    Correct Documentation

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Author: Glorious Oyovwuvwe Ohwojeheri <gloriousohwojeheri608@gmail.com>
-Purpose: Define a Square class
+Purpose: Define a singly linked lista and its node class
 """
 
 
